f0cc3850dc45c2c9a8e3e594d41d4b718a523f08
3x1b <137458387+3x1b@users.noreply.github.com>
2025-02-21 15:53:39 +0000

    Create index.tsx

---
+++ plugins/customAppIcon/index.tsx
@@ -0,0 +1,39 @@
+import { OptionType } from "@utils/types";
+import { definePluginSettings } from "@api/Settings";
+import definePlugin from "@utils/types";
+
+const settings = definePluginSettings({
+    customIconURL: {
+        type: OptionType.STRING,
+        description: "The link to the custom icon image you want to use!",
+        default: "https://i.pinimg.com/736x/da/55/b7/da55b7cd90942123eef642f922a12a1e.jpg"
+    }
+});
+
+const observer = new MutationObserver((mutationsList, observer) => {
+    for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            document.querySelector('img[alt="App Icon"]').src = settings.store.customIconURL;
+        }
+    }
+});
+
+export default definePlugin({
+    name: "CustomAppIcon",
+    description: "A plugin that changes that discord logo in the top-right corner of your screen into whatever you please!",
+    authors: [
+        {
+            name: "3x1b",
+            id: 800108516626661396n
+        }
+    ],
+
+    settings,
+
+    start() {
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    }
+});
