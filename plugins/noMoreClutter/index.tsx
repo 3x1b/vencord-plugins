51c948a9cb89960a793ef00b8e302cf682b752b0
3x1b <137458387+3x1b@users.noreply.github.com>
2025-02-21 15:52:50 +0000

    Update index.tsx
    added noMutuals option and completely restructured the plugin basically idk what im doing PLEASE HELP ME AAAAAAAAA

--- plugins/noMoreClutter/index.tsx
+++ plugins/noMoreClutter/index.tsx
@@ -2,17 +2,59 @@ import { OptionType } from "@utils/types";
 import { definePluginSettings } from "@api/Settings";
 import definePlugin from "@utils/types";
 
+const noMutualObserver = new MutationObserver((mutationsList, observer) => {
+    for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            const mutualRegex = /Mutual (Friends|Servers)/g;
+            const h2Elements = document.querySelectorAll('h2');
+            for (let h2 of h2Elements) {
+                if (mutualRegex.test(h2.innerHTML.trim())) {
+                    h2.parentElement?.parentElement?.parentElement?.remove()
+                }
+            }
+        }
+    }
+});
+
 const cssTexts = {
-    // On by default
     "noGiftButton": `button[aria-label="Send a gift"] * { display: none !important; }`,
     "noShopButton": `a[href="/shop"] * { display: none !important; }`,
     "noProfileDecorations": `svg[class^="avatarDecoration"] { display: none !important; }`,
     "noPlayAgainTab": `ul[aria-label="Direct Messages"] div[class^="container"] { display: none !important; }`,
     "noBadges": `div[aria-label="User Badges"] * { display: none !important; }`,
-
-    // Off by default
     "noPronouns": `div[aria-label="Pronouns"] * { display: none !important; } div[class^=dotSpacer] { display: none !important; }`,
-    "noNitroButton": `a[href="/store"] * { display: none !important; }`
+    "noNitroButton": `a[href="/store"] * { display: none !important; }`,
+    "noMutuals": `div[aria-label$="Mutual Friends"] { display: none !important; } div[aria-label$="Mutual Servers"] { display: none !important; }`
+}
+
+const activateScripts = {
+    "noGiftButton": function() { applyCSS("noGiftButton"); },
+    "noShopButton": function() { applyCSS("noShopButton"); },
+    "noProfileDecorations": function() { applyCSS("noProfileDecorations"); },
+    "noPlayAgainTab": function() { applyCSS("noPlayAgainTab"); },
+    "noBadges": function() { applyCSS("noBadges"); },
+    "noPronouns": function() { applyCSS("noPronouns"); },
+    "noNitroButton": function() { applyCSS("noNitroButton"); },
+    "noMutuals": function() {
+            applyCSS("noMutuals");
+            noMutualObserver.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
+        }
+}
+
+function applyCSS(id) {
+    const style = document.createElement('style');
+    style.classList.add("NoMoreClutter");
+    style.textContent = cssTexts[id];
+    document.head.appendChild(style);
+}
+
+function removeAllCSS() {
+    document.querySelectorAll(".NoMoreClutter").forEach(function(style) {
+        style.remove();
+    })
 }
 
 const settings = definePluginSettings({
@@ -46,6 +88,10 @@ const settings = definePluginSettings({
     noNitroButton: {
         type: OptionType.BOOLEAN,
         default: false
+    },
+    noMutuals: {
+        type: OptionType.BOOLEAN,
+        default: false
     }
 });
 
@@ -56,20 +102,14 @@ export default definePlugin({
     settings: settings,
 
     start() {
-        for (const [i, v] of Object.entries(cssTexts)) {
-            if (!settings.store[i]) {
-                continue;
+        for (const [i, _] of Object.entries(cssTexts)) {
+            if (settings.store[i]) {
+                activateScripts[i]();
             }
-            const style = document.createElement('style');
-            style.classList.add("NoMoreClutter");
-            style.textContent = v;
-            document.head.appendChild(style);
         }
     },
     
     stop() {
-        document.querySelectorAll(".NoMoreClutter").forEach(function(style) {
-            style.remove();
-        })
+        removeAllCSS();
     }
 });
